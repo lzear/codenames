96c47d2424aecf83c3c4535bde07abcde9cc56c7
lzear <antoine.clausse@gmail.com>
2021-06-10 09:16:39 +0200

    Update dependencies

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 it('renders "Generate" button', () => {
+  // @ts-ignore
+  global.window = { location: { search: '?seed=abc' } };
   const { getByText } = render(
     <MemoryRouter>
       <App />
@@ -11,4 +14,6 @@ it('renders "Generate" button', () => {
   );
   const linkElement = getByText(/New game/i);
   expect(linkElement).toBeInTheDocument();
+
+  userEvent.click(linkElement);
 });
