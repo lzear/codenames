2f1131dff7f685872736f87777c34ce24160e2e9
lzear <antoine.clausse@gmail.com>
2021-06-10 09:16:39 +0200

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   env: { browser: true, node: true, es6: true },
   settings: { react: { version: 'detect' } },
-  "parserOptions": {
-    "project": ["tsconfig.json"]
+  parserOptions: {
+    project: ['./tsconfig.json'],
   },
   extends: [
     'airbnb-typescript',
@@ -20,6 +20,15 @@ module.exports = {
       { allowExpressions: true },
     ],
   },
-  overrides: [{ files: ['**/*.test.tsx'], env: { jest: true } }],
+
+  overrides: [
+    { files: ['**/*.test.tsx'], env: { jest: true } },
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      parserOptions: {
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+    },
+  ],
   plugins: ['@typescript-eslint', 'prettier', 'react-app', 'react-hooks'],
 };
--- src/Image.tsx
+++ src/Image.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSpring, animated, interpolate } from 'react-spring';
+import { useSpring, animated } from 'react-spring';
 import { useGesture } from 'react-use-gesture';
 import { Color } from './generate';
 import { Tiles } from './Game';
@@ -12,32 +12,20 @@ const Image: React.FC<{
   img: string;
   seed: string;
 }> = ({ seed, game, width, hidden, img, locked }) => {
-  const [{ x, y, size, angle }, set] = useSpring<{
-    x: number;
-    y: number;
-    size: number;
-    angle: number;
-  }>(() => ({
+  const [{ x, y, scale, rotateZ }, set] = useSpring(() => ({
     x: 0,
     y: 0,
-    size: 1,
-    angle: 0,
+    scale: 1,
+    rotateZ: 0,
   }));
   const bind = useGesture(
     {
       onDrag: ({ offset: [xx, yy] }) => set({ x: xx, y: yy }),
-      onPinch: ({ memo, first, da }) => {
-        if (first) {
-          return [
-            da[0] ? size.getValue() / da[0] : size.getValue(),
-            angle.getValue() - da[1],
-          ];
-        }
+      onPinch: ({ offset }) => {
         set({
-          angle: memo[1] + da[1],
-          size: da[0] * memo[0],
+          rotateZ: offset[1],
+          scale: offset[0] / 100,
         });
-        return memo;
       },
     },
     { drag: { delay: 0 } },
@@ -54,11 +42,10 @@ const Image: React.FC<{
             width: '100%',
             zIndex: -1,
             touchAction: 'none',
-            transform: interpolate(
-              [x, y, angle, size],
-              (xx, yy, aa, s) =>
-                `translate3d(${xx}px,${yy}px,0) rotate(${aa}deg) scale(${s})`,
-            ),
+            scale,
+            rotateZ,
+            x,
+            y,
           }}
         />
       )}
